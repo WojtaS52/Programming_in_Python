f0c15626d72f3f5c809dc027735d6e5f54927a0f
Wojciech Świderski <wojciechiderski@gmail.com>
2023-12-01 17:54:17 +0100

    added classes wolf and sheep

---
+++ Assignment2/class/sheep.py
@@ -0,0 +1,24 @@
+import random
+
+
+class Sheep:
+    def __init__(self, sid, x, y, move_dist):
+        self.sid = sid
+        self.x = x
+        self.y = y
+        self.move_dist = move_dist
+        self.is_live = True
+
+    def sheep_move(self, dist):
+        # TODO: change on like in instruction up ~ north, ect?
+
+        direct = random.choice(['up', 'down', 'left', 'right'])
+
+        if direct == 'up':
+            self.y = self.y + dist
+        elif direct == 'down':
+            self.y = self.y - dist
+        elif direct == 'left':
+            self.x = self.x - dist
+        elif direct == 'right':
+            self.x = self.x + dist
---
+++ Assignment2/class/wolf.py
@@ -0,0 +1,29 @@
+import math
+
+
+class Wolf:
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+
+    def distance_to_the_nearest_sheep(self, sheep):
+        dist = math.sqrt(((sheep.x - self.x)**2) + ((sheep.y - self.y)**2))
+        return dist
+
+    def move_wolf(self, sheeps_list, wolf_step):
+        nearest_sheep = min(sheeps_list)
+
+        if nearest_sheep:
+
+            dist_to_sheep = self.distance_to_the_nearest_sheep(nearest_sheep)
+
+            # Check if sheep is in available are for attack :C
+
+            if wolf_step > dist_to_sheep:
+                self.x = nearest_sheep.x
+                self.y = nearest_sheep.y
+                nearest_sheep.is_live = False       # :----(
+            else:
+                ratio = wolf_step / dist_to_sheep
+                self.x += ratio * (nearest_sheep.x - self.x)
+                self.y += ratio * (nearest_sheep.y - self.y)
