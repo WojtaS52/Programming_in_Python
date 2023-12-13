ef836f87872e6d933330a2a3b5be3e6d0a1364fa
Wojciech Świderski <wojciechiderski@gmail.com>
2023-12-13 10:38:38 +0100

    go go power rangers

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,7 +1,7 @@
 from simulation import Simulation
 
 if __name__ == '__main__':
-    num_sheep = 10
+    num_sheep = 15
     limit = 10.0
     wolf_movement = 1.0
     sheep_movement = 0.5
@@ -9,4 +9,3 @@ if __name__ == '__main__':
 
     simulation = Simulation(num_sheep, limit, wolf_movement, sheep_movement, max_rounds)
     simulation.run()
-
